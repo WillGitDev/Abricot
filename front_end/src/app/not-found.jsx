676e1f8cf3f34abf88222dfb7cd20721c6997aa2
WillGitDev <w.grondin@orange.fr>
2026-06-16 14:42:43 +0400

    Implémentation de la page 404

---
+++ front_end/src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import styles from './notfound.module.css';
+import Link from 'next/link';
+
+export default function notFound() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>404 Page non trouvée</h1>
+            <Link href="/" className={styles.link}>
+                Retour à l'acceuil
+            </Link>
+        </div>
+    );
+}
